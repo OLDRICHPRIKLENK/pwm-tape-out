72d40792de74b29b6c10f666bad04268cd6c99a0
Oldřich Příklenk <oprik@KPCNOTE14.artex.local>
2026-05-03 16:06:50 +0200

    changed test.py

--- test/test.py
+++ test/test.py
@@ -1,40 +1,26 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import Timer
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+async def test_pwm(dut):
+    # Generate a 100MHz clock
+    clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
+    # Initialize inputs
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    
+    # Release Reset after 100ns
+    await Timer(100, units="ns")
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # Replicate your switch logic. If sw[1:0] is mapped to ui_in[2:1]:
+    dut.ui_in.value = 0b00000000 # Set switches to 00
+    await Timer(100, units="us") # Wait 100 microseconds
+    
+    dut.ui_in.value = 0b00000010 # Flip switch 1 UP
+    await Timer(100, units="us")
